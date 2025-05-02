0d6fd7b822c716b74f1329af3b7aa7417b408ec5
Emmanuel Guerra <emmanuelguerra1945@gmail.com>
2025-05-01 22:01:02 -0300

    Update PlaylistControl.java
    renomeação de alguns atributos

--- src/control/PlaylistControl.java
+++ src/control/PlaylistControl.java
@@ -1,35 +1,39 @@
 package control;
 
+import model.Music;
 import model.Playlist;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class PlaylistControl extends Playlist {
+public class PlaylistControl extends Music {
 
-    Scanner scn = new Scanner(System.in);
-    List<String> playlist = new ArrayList<>();
+    Playlist playlist = new Playlist();
+    Scanner scn = new Scanner (System.in);
 
-    public void createPlaylist() {
+    public void CreatePlaylist() {
 
+        List<String> playlist = new ArrayList<>();
+
+        Scanner scn = new Scanner(System.in);
         System.out.print("Informe o nome da sua playlist: ");
-        setNomeMusica(scn.nextLine());
-//      playlist.add("Playlist  " + setNomeMusica());
+        setSongName(scn.nextLine());
+//        playlist.add("Playlist  " + setSongName());
 
         System.out.println("Informe agora os detalhes das músicas que deseja inserir na Playlist");
 
         System.out.print("\nNome da música = ");
-        setNomeMusica(scn.nextLine()); //Inserção direta de string para parametro do metodo da class Music
-        playlist.add("Nome da música = " + getNomeMusica()); //list recebe 2 strings (gets retornam string tbm)
+        setSongName(scn.nextLine()); //Inserção direta de string para parametro do metodo da class Music
+        playlist.add("Nome da música = " + getSongName()); //list recebe 2 strings (gets retornam string tbm)
 
         System.out.print("Artista = ");
-        setArtista(scn.nextLine());
-        playlist.add("Artista = " + getArtista());
+        setArtist(scn.nextLine());
+        playlist.add("Artista = " + getArtist());
 
         System.out.print("Genero = ");
-        setGenero(scn.nextLine());
-        playlist.add("Genero = " + getGenero());
+        setGenre(scn.nextLine());
+        playlist.add("Genero = " + getGenre());
 
         System.out.print("Album = ");
         setAlbum(scn.nextLine());
@@ -37,7 +41,7 @@ public class PlaylistControl extends Playlist {
 
         System.out.println("\nPlaylist Criada! ");
 
-        for (String playlists : playlist) {
+        for (String playlists: playlist) {
 
             System.out.println(playlists);
 
@@ -48,7 +52,8 @@ public class PlaylistControl extends Playlist {
 
     }
 
-    public void editPlaylist() {
+
+    public void EditPlaylist() {
 
     }
 
