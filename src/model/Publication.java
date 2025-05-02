ec7ff61ccef2d1a0ef3be4bd40d7b6c5ac1abe43
Emmanuel Guerra <emmanuelguerra1945@gmail.com>
2025-05-01 22:02:38 -0300

    Update Publication.java
    renomeação de atributos

--- src/model/Publication.java
+++ src/model/Publication.java
@@ -1,48 +1,58 @@
 package model;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Publication {
 
+    private List<String> publication;
     private int publicationID;
-    private String descricao;
-    private int avaliacao;
+
+    private String description;
+    private int rating;
     private boolean like;
 
-    public Publication(int PublicationID, String Descricao, int Avaliacao, boolean Like) {
+//    public Publication(List<String> publication,int PublicationID,String description,int rating,boolean Like) {
+//        this.publication = publication;
+//        this.publicationID = publicationID;
+//        this.description = description;
+//        this.rating = rating;
+//        this.like = like;
+//    }
+
+    public Publication(){
+
+    }
+    public List<String> getPublication(){
+        return publication;
+    }
+
+    public void setPublicationID(int publicationID){
         this.publicationID = publicationID;
-        this.descricao = descricao;
-        this.avaliacao = avaliacao;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+     public String getDescription(){
+        return description;   //Comentário da publicação
+     }
+
+    public void setRating(int rating){
+        this.rating = rating;
+    }
+
+    public int getRating(){
+        return rating;   //Avaliação da publicação
+    }
+
+    public void setLike(boolean like){
         this.like = like;
     }
 
-    public Publication() {
-
-    }
-
-    public void setPublicationID(int publicationID) {
-        this.publicationID = publicationID;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setAvaliacao(int avaliacao) {
-        this.avaliacao = avaliacao;
-    }
-
-    public int getAvaliacao() {
-        return avaliacao;
-    }
-
-    public void setLike(boolean like) {
-        this.like = like;
-    }
-
-    public boolean getLike() {
-        return like;
+    public boolean getLike(){
+        return like;    //Curtida da publicação
     }
 }
